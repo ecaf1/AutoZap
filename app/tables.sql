259dbe35fdbf9ea47205e93170ba1c9c12930e25
Edeilson Azevedeo <edeilsonazevedo22@gmail.com>
2024-05-21 18:03:46 -0300

    fix: refactore database

--- app/tables.sql
+++ app/tables.sql
@@ -1,26 +1,19 @@
 CREATE TABLE IF NOT EXISTS users(
-  phone PRIMARY KEY VARCHAR(20) NOT NULL UNIQUE,
-  nome  VARCHAR(20) NOT NULL,
+  phone VARCHAR(20) PRIMARY KEY  NOT NULL UNIQUE,
+  name  VARCHAR(20) NOT NULL,
   description  VARCHAR(50)
-);
-
-CREATE TABLE IF NOT EXISTS user_updates(
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_phone  VARCHAR(20) NOT NULL,
-  update DATETIME NOT NULL,
-  FOREIGN KEY (user_phone) REFERENCES users.phone
 );
 CREATE TABLE IF NOT EXISTS messages(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_phone VARCHAR(20),
   message TEXT,
-  recepit_date DATETIME,
-  FOREIGN KEY user_phone REFERENCES users.phone
+  receipt_date DATETIME,
+  FOREIGN KEY (user_phone) REFERENCES users(phone)
 );
 
 CREATE TABLE IF NOT EXISTS conversations(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_phone VARCHAR(20),
   status VARCHAR(20),
-  FOREIGN KEY user_phone REFERENCES users.phone
+  FOREIGN KEY (user_phone) REFERENCES users(phone)
 );
